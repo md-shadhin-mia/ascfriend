d58ef3a6ce8f5abdc4cd5070ab3efbc01ab29b42
muhammadshadhin <mdshadhinkn@gmail.com>
2021-07-26 10:07:29 +0000

    style ass in material ui

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,8 +1,14 @@
 import React from "react";
-import { Typography } from "@material-ui/core";
+import { Button, makeStyles, Typography } from "@material-ui/core";
+const style = makeStyles({
+    homeButtons:{
+        margin:"4px"
+    }
+})
 
 function Home()
 {
+    const classes = style();
     return (
         <div>
             <Typography variant="h2">
@@ -11,6 +17,12 @@ function Home()
             <Typography variant="h4" >
                 You Have to Access Evething Here.
             </Typography>
+            <Button variant="contained" color="primary" className={classes.homeButtons}>
+                Join Now
+            </Button>
+            <Button variant="outlined" color="primary" className={classes.homeButtons}>
+                Learn More
+            </Button>
         </div>
     )
 }
